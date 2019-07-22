01663c9bcff3d2cbf465fd4c9885d10e5699207b
kuruwan <53120242+kuruwan@users.noreply.github.com>
2019-07-22 23:45:32 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,5 +7,5 @@ A simple Farmware example that tells FarmBot to log a new message including the 
 
 from farmware_tools import get_config_value, device
 
-INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
+INPUT_VALUE = get_pin_value(63)
 device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
