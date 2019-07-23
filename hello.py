d92a92c05022529408bd86d106c465180121a891
kuruwan <53120242+kuruwan@users.noreply.github.com>
2019-07-23 22:42:12 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -11,5 +11,5 @@ temp = {{pin63}}
 from farmware_tools import get_config_value, device
 
 INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
-INPUT_VALUE2 = get_config_value(farmware_name='Hello Farmware Input2', config_name='temp', value_type=str)
+
 device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
